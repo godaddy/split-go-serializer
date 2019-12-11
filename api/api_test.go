aa5f1df3832a63216356166931e2a965c6e12e96
Ping-Jou Chiang <pchiang@godaddy.com>
2019-12-11 15:07:40 -0800

    add missing api files

---
+++ api/api_test.go
@@ -0,0 +1,38 @@
+package api
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+const testKey = "someKey"
+
+func TestNewSplitioAPIBindingValid(t *testing.T) {
+	result := NewSplitioAPIBinding(testKey)
+	assert.EqualValues(t, testKey, result.splitioAPIKey)
+}
+
+func TestHTTPGetReturnsError(t *testing.T) {
+
+	result := NewSplitioAPIBinding(testKey)
+	err := result.HTTPGet()
+
+	assert.EqualError(t, err, "not implemented")
+}
+
+func TestGetSegmentChangesReturnsError(t *testing.T) {
+
+	result := NewSplitioAPIBinding(testKey)
+	err := result.GetSegmentChanges()
+
+	assert.EqualError(t, err, "not implemented")
+}
+
+func TestGetSplitChangesReturnsError(t *testing.T) {
+
+	result := NewSplitioAPIBinding(testKey)
+	err := result.GetSplitChanges()
+
+	assert.EqualError(t, err, "not implemented")
+}
