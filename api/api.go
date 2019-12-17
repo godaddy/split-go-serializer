49d5c906bfe6ca50b848e099ef9f39851b928600
Ping-Jou Chiang <46336255+wendychiang@users.noreply.github.com>
2019-12-17 09:28:10 -0800

    feat(splitio-api-wrapper): implement httpGet function (#2)

--- api/api.go
+++ api/api.go
@@ -1,20 +1,71 @@
 package api
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"strconv"
+	"strings"
+
+	"github.com/go-resty/resty/v2"
+)
+
+const splitioAPIUri = "https://sdk.split.io/api"
 
 // SplitioAPIBinding contains splitioAPIKey
 type SplitioAPIBinding struct {
 	splitioAPIKey string
+	splitioAPIUri string
 }
 
 // NewSplitioAPIBinding returns a new SplitioAPIBinding
-func NewSplitioAPIBinding(splitioAPIKey string) *SplitioAPIBinding {
-	return &SplitioAPIBinding{splitioAPIKey}
+func NewSplitioAPIBinding(apiKey string, apiURL string) *SplitioAPIBinding {
+	if apiURL == "" {
+		apiURL = splitioAPIUri
+	}
+	return &SplitioAPIBinding{apiKey, apiURL}
 }
 
-// HTTPGet will make a GET request to the Split.io SDK API
-func (binding *SplitioAPIBinding) HTTPGet() error {
-	return fmt.Errorf("not implemented")
+// httpGet makes a GET request to the Split.io SDK API.
+// path is the path of the HTTP request, either "splitChanges" or "segmentChanges"
+// segment is the segment name when path is "segmentChange", otherwise should be empty
+// since is an integer used as a query string, will be -1 on the first request
+func (binding *SplitioAPIBinding) httpGet(path string, segment string, since int) (map[string]interface{}, error) {
+	client := resty.New()
+	resp, err := client.R().
+		SetPathParams(map[string]string{
+			"path":    path,
+			"segment": segment,
+		}).
+		SetQueryParams(map[string]string{
+			"since": strconv.Itoa(since),
+		}).
+		SetHeaders(map[string]string{
+			"Accept":        "application/json",
+			"Authorization": fmt.Sprintf("Bearer %s", binding.splitioAPIKey),
+		}).
+		Get(fmt.Sprintf("%s/{path}/{segment}", binding.splitioAPIUri))
+
+	if err != nil {
+		err = fmt.Errorf("Http get request error: %s", err)
+		return map[string]interface{}{}, err
+	}
+
+	if resp.StatusCode() != http.StatusOK {
+		err = fmt.Errorf("Non-OK HTTP status: %s", resp.Status())
+		return map[string]interface{}{}, err
+	}
+
+	var data map[string]interface{}
+	decoder := json.NewDecoder(strings.NewReader(string(resp.Body())))
+	decoder.UseNumber()
+	err = decoder.Decode(&data)
+	if err != nil {
+		err = fmt.Errorf("Decode error: %s", err)
+		return map[string]interface{}{}, err
+	}
+
+	return data, nil
 }
 
 // GetSegmentChanges will get segment data
