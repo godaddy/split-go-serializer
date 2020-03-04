26091eac9b2dc1acece9725b94a33fe7e6ee2771
Jacob Waterman <jwaterman@godaddy.com>
2020-03-04 09:13:27 -0800

    refactor(getSerializedData): BREAKING CHANGE move serialization to poller and cache the results. GetSerializedData method added to Fetcher Interface

--- poller/poller.go
+++ poller/poller.go
@@ -1,6 +1,9 @@
 package poller
 
 import (
+	"encoding/json"
+	"fmt"
+	"reflect"
 	"sync/atomic"
 	"time"
 	"unsafe"
@@ -9,11 +12,16 @@ import (
 	"github.com/splitio/go-client/splitio/service/dtos"
 )
 
+const emptyCacheLoggingScript = `<script>window.__splitCachePreload = {}</script>`
+
+const formattedLoggingScript = `<script>window.__splitCachePreload = { splitsData: %v, since: %v, segmentsData: %v, usingSegmentsCount: %v }</script>`
+
 // Fetcher is an interface contains GetSplitData, Start and Stop functions
 type Fetcher interface {
 	Start()
 	Stop()
 	GetSplitData() SplitData
+	GetSerializedData() string
 }
 
 // Poller implements Fetcher and contains cache pointer, splitio, and required info to interact with aplitio api
@@ -34,6 +42,20 @@ type SplitData struct {
 	UsingSegmentsCount int
 }
 
+// Cache contains raw split data as well as the data in serialized format
+type Cache struct {
+	SplitData
+	SerializedData string
+}
+
+// SplitCachePreload does something
+type SplitCachePreload struct {
+	Since              int64
+	UsingSegmentsCount int
+	SplitsData         string
+	SegmentsData       string
+}
+
 // NewPoller returns a new Poller
 func NewPoller(splitioAPIKey string, pollingRateSeconds int, serializeSegments bool, splitio api.Splitio) *Poller {
 	if pollingRateSeconds == 0 {
@@ -42,7 +64,11 @@ func NewPoller(splitioAPIKey string, pollingRateSeconds int, serializeSegments b
 	if splitio == nil {
 		splitio = api.NewSplitioAPIBinding(splitioAPIKey, "")
 	}
-	return &Poller{make(chan error), splitio, pollingRateSeconds, serializeSegments, make(chan bool), unsafe.Pointer(&SplitData{})}
+	emptyCache := Cache{
+		SplitData:      SplitData{},
+		SerializedData: emptyCacheLoggingScript,
+	}
+	return &Poller{make(chan error), splitio, pollingRateSeconds, serializeSegments, make(chan bool), unsafe.Pointer(&emptyCache)}
 }
 
 // pollForChanges updates the Cache with latest splits and segment
@@ -65,19 +91,33 @@ func (poller *Poller) pollForChanges() {
 	}
 
 	// Update Cache
-	updatedCache := SplitData{
+	splitData := SplitData{
 		Splits:             splits,
 		Since:              since,
 		Segments:           segments,
 		UsingSegmentsCount: usingSegmentsCount,
 	}
+	serializedData, err := generateSerializedData(splitData)
+	if err != nil {
+		poller.Error <- err
+		return
+	}
+	updatedCache := Cache{
+		SplitData:      splitData,
+		SerializedData: serializedData,
+	}
 	atomic.StorePointer(&poller.cache, unsafe.Pointer(&updatedCache))
 
 }
 
-// GetSplitData returns cache results of SplitData
+// GetSplitData returns split data cache results
 func (poller *Poller) GetSplitData() SplitData {
-	return *(*SplitData)(atomic.LoadPointer(&poller.cache))
+	return (*(*Cache)(atomic.LoadPointer(&poller.cache))).SplitData
+}
+
+// GetSerializedData returns split data cache results
+func (poller *Poller) GetSerializedData() string {
+	return (*(*Cache)(atomic.LoadPointer(&poller.cache))).SerializedData
 }
 
 // Start creates a goroutine and keep tracking until it stops
@@ -103,4 +143,39 @@ func (poller *Poller) jobs() {
 			poller.pollForChanges()
 		}
 	}
+}
+
+func generateSerializedData(splitData SplitData) (string, error) {
+	if reflect.DeepEqual(splitData, SplitData{}) {
+		return emptyCacheLoggingScript, nil
+	}
+	splitsData := map[string]string{}
+
+	// Serialize values for splits
+	for _, split := range splitData.Splits {
+		marshalledSplit, _ := json.Marshal(split)
+		splitsData[split.Name] = string(marshalledSplit)
+	}
+
+	marshalledSplits, err := json.Marshal(splitsData)
+	if err != nil {
+		return "", err
+	}
+
+	segmentsData := map[string]string{}
+
+	// Serialize values for segments
+	for _, segment := range splitData.Segments {
+		marshalledSegment, _ := json.Marshal(segment)
+		segmentsData[segment.Name] = string(marshalledSegment)
+	}
+
+	marshalledSegments, err := json.Marshal(segmentsData)
+	if err != nil {
+		return "", err
+	}
+
+	splitCachePreload := &SplitCachePreload{splitData.Since, splitData.UsingSegmentsCount, string(marshalledSplits), string(marshalledSegments)}
+
+	return fmt.Sprintf(formattedLoggingScript, splitCachePreload.SplitsData, splitCachePreload.Since, splitCachePreload.SegmentsData, splitCachePreload.UsingSegmentsCount), nil
 }
--- serializer/serializer.go
+++ serializer/serializer.go
@@ -1,24 +1,8 @@
 package serializer
 
 import (
-	"encoding/json"
-	"fmt"
-	"reflect"
-
 	"github.com/godaddy/split-go-serializer/v2/poller"
 )
-
-const emptyCacheLoggingScript = `<script>window.__splitCachePreload = {}</script>`
-
-const formattedLoggingScript = `<script>window.__splitCachePreload = { splitsData: %v, since: %v, segmentsData: %v, usingSegmentsCount: %v }</script>`
-
-// SplitCachePreload does something
-type SplitCachePreload struct {
-	Since              int64
-	UsingSegmentsCount int
-	SplitsData         string
-	SegmentsData       string
-}
 
 // Serializer contains poller
 type Serializer struct {
@@ -32,37 +16,6 @@ func NewSerializer(poller poller.Fetcher) *Serializer {
 
 // GetSerializedData serializes split and segment data into strings
 func (serializer *Serializer) GetSerializedData() (string, error) {
-	latestData := serializer.poller.GetSplitData()
-	if reflect.DeepEqual(latestData, poller.SplitData{}) {
-		return emptyCacheLoggingScript, nil
-	}
-	splitsData := map[string]string{}
-
-	// Serialize values for splits
-	for _, split := range latestData.Splits {
-		marshalledSplit, _ := json.Marshal(split)
-		splitsData[split.Name] = string(marshalledSplit)
-	}
-
-	marshalledSplits, err := json.Marshal(splitsData)
-	if err != nil {
-		return "", err
-	}
-
-	segmentsData := map[string]string{}
-
-	// Serialize values for segments
-	for _, segment := range latestData.Segments {
-		marshalledSegment, _ := json.Marshal(segment)
-		segmentsData[segment.Name] = string(marshalledSegment)
-	}
-
-	marshalledSegments, err := json.Marshal(segmentsData)
-	if err != nil {
-		return "", err
-	}
-
-	splitCachePreload := &SplitCachePreload{latestData.Since, latestData.UsingSegmentsCount, string(marshalledSplits), string(marshalledSegments)}
-
-	return fmt.Sprintf(formattedLoggingScript, splitCachePreload.SplitsData, splitCachePreload.Since, splitCachePreload.SegmentsData, splitCachePreload.UsingSegmentsCount), nil
+	serializedData := serializer.poller.GetSerializedData()
+	return serializedData, nil
 }
